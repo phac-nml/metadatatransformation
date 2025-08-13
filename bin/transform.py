d2573b732482d329d2d2f1a086e4838d570e8144
Connor Chato <cchato@cscscience.ca>
2025-08-13 14:52:29 -0500

    Linting Fixes follow up

--- bin/transform.py
+++ bin/transform.py
@@ -336,7 +336,7 @@ def main():
         remove_all_NA_columns(metadata_irida)
         metadata_readable.to_csv(RESULTS_PATH, index=False)
         metadata_irida.to_csv(TRANSFORMATION_PATH, index=False)
-    
+
     elif (args.transformation == CATEGORIZE):
 
         metadata_readable, metadata_irida = categorize(metadata)
