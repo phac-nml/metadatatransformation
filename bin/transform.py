c818574859a3e31293ce56b4dc8b1f9f9da139b3
Connor Chato <cchato@cscscience.ca>
2025-08-14 15:24:49 -0500

    Add apply function

--- bin/transform.py
+++ bin/transform.py
@@ -189,6 +189,8 @@ def categorize(metadata):
         else:
             return UNKNOWN_VALUE
 
+    metadata_readable["calc_source_type"] = metadata_readable.apply(categorize_row, axis = 1)
+
     metadata_irida = metadata_readable[[SAMPLE_HEADER, "calc_source_type"]].copy(deep=True)
 
     return metadata_readable, metadata_irida
